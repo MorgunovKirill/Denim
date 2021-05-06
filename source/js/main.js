fe72a737f2f6f9340092ca2bb513c2b936cee736
Kirill <kirillmorgunov@mail.ru>
2021-05-06 14:29:08 +0300

    Добавляет nouislider

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
 import {animate} from './modules/animate';
 import {initAccordion} from './modules/accordion';
+import {initRange} from './modules/init-range';
 
 animate();
 initAccordion();
+initRange();
